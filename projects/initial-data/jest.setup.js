8f8b2cbf36cb2fb87cb763f0bc160d41b52c0443
Ben Hutton <relequestual@gmail.com>
2024-06-21 11:17:10 +0100

    Improve testing by checking a file is created wit hthe correct value as opposed to checking a function waw called

--- projects/initial-data/jest.setup.js
+++ projects/initial-data/jest.setup.js
@@ -1,9 +1,8 @@
 /* eslint-disable no-undef */
 import { server } from './mocks/server';
-import { vol } from 'memfs';
-beforeAll(() => server.listen());
-beforeEach(() => {
-  vol.reset();
+
+beforeAll(() => {
+  server.listen();
 });
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
--- projects/initial-data/processRespository.test.js
+++ projects/initial-data/processRespository.test.js
@@ -1,31 +1,67 @@
-/* eslint-disable no-undef */
+import { tmpdir } from 'node:os';
+import { sep } from 'node:path';
+import fs from 'fs';
+
 import { fetchRepoCreationDate } from './main.js';
 import { getInput } from './setup.js';
 import { Octokit } from 'octokit';
 import { DataRecorder } from './dataRecorder.js';
-import fs from 'fs';
+import { describe, it, expect, afterEach, afterAll } from '@jest/globals';
+
 const { token } = getInput();
 const octokit = new Octokit({ auth: token });
+const tmpDir = tmpdir();
+const tmpDirForTests = fs.mkdtempSync(`${tmpDir}${sep}`);
 
-
-describe('Skretch Objective', () => {
-  let data;
-  it('receives a mocked response from the GitHub API', async () => {
-    data = await fetchRepoCreationDate(octokit, 'octocat', 'hello-world');
-    expect(data).toEqual(1550934514000);
-  
-  
+describe('Basic tests', () => {
+  afterEach((done) => {
+    fs.rm(tmpDirForTests, { recursive: true, force: true }, (err) => {
+      if (err) {
+        console.log(err);
+        done(err);
+      } else {
+        console.debug('temp directory cleared');
+        fs.mkdir(tmpDirForTests, {}, (err) => {
+          if (err) {
+            console.log(err);
+            done(err);
+          } else {
+            done();
+          }
+        });
+      }
+    });
   });
 
-  it('writes JSON data to csv file', async() => {
-    const response = {
-      'date_first_commit': 1550934514000,
-    };
-    const spy = jest.spyOn(DataRecorder.prototype, 'appendToCSV').mockImplementation(() => fs.appendFileSync('test.csv', '1550934514000', 'utf8'));
-    const dataRecorder = new DataRecorder('test.csv');
-    dataRecorder.appendToCSV([response.date_first_commit]);
+  afterAll((done) => {
+    fs.rm(tmpDirForTests, { recursive: true, force: true }, (err) => {
+      if (err) {
+        throw err;
+      }
 
-    expect(spy).toHaveBeenCalled();
-    expect(spy).toHaveBeenCalledWith([data]); 
+      console.log(`${tmpDirForTests} is deleted!`);
+      done();
+    });
+  });
+
+  it('Simple use of Octokit calls the GitHub API', async () => {
+    let data;
+    data = await fetchRepoCreationDate(octokit, 'octocat', 'hello-world');
+    expect(data).toEqual(1550934514000);
+  });
+
+  it('DataRecorder writes JSON data to csv file', async () => {
+    const response = {
+      date_first_commit: 1550934514000,
+    };
+    const fileName = `${tmpDirForTests}${sep}test.csv`;
+
+    const dataRecorder = new DataRecorder(fileName);
+    dataRecorder.appendToCSV([response.date_first_commit]);
+    const fileContent = fs.readFileSync(fileName, 'utf8');
+
+    expect(fileContent).toEqual(
+      expect.stringContaining(response.date_first_commit.toString()),
+    );
   });
 });
